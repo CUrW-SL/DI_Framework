869520ab796efa41888cae82fdd17b6d53be40ba
madumalt <madumalt@gmail.com>
2018-12-13 16:14:54 +0530

    Adding dependency links

--- setup.py
+++ setup.py
@@ -10,5 +10,9 @@ setup(
     author_email='madumalt@gamil.com',
     description='Data integration framework with algo_wrapper conformity.',
     install_requires=['data_layer', 'algo_wrapper', 'pandas', 'numpy', 'netCDF4'],
+    dependency_links=[
+        'git+https://github.com/CUrW-SL/data_layer.git@master#egg=data_layer-0.0.1',
+        'git+https://github.com/CUrW-SL/algo_wrapper.git@master#egg=algo_wrapper-1.0.0'
+    ],
     zip_safe=True
 )
